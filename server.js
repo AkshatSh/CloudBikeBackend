5c869cbb7097ed2fbbe050345a7a0eab990bfead
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2015-10-18 15:20:17 -0700

    random

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ function storeMenuData(_db, collection, _req, _res, data, callback) {
 }
 
 function insertDocument(_db, collection, _req, _res, data, callback) {
-	console.log(data);
+	console.log(data[0]);
     _db.collection(collection).insert(data,
         function(err, result) {
             if(err)
