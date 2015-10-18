789a147180a534dd1bd980785d1aa24fbf09e84d
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2015-10-18 14:20:29 -0700

    a

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ router.route('/route')
 
 
 		insertDocument(db, 'documents', req, res, recievedData, function (err, data) {
-			//storeMenuData(db, 'routes', req, res, {data}, function () {});
+			storeMenuData(db, 'routes', req, res, data, function () {});
 		});
 
 
